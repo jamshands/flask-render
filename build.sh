4f9e3be2e98ffc9041e4cdfa31f98791452a5104
jamshands <108704715+jamshands@users.noreply.github.com>
2025-02-21 15:17:49 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Render 배포 시 Tesseract-OCR 강제 설치
-apt-get update && apt-get install -y tesseract-ocr libtesseract-dev
+apt-get update && apt-get install -y tesseract-ocr libtesseract-dev libleptonica-dev
 
 # Tesseract 실행 경로 확인 (설치된 위치 출력)
 echo "✅ Tesseract 설치 완료, 실행 경로 확인:"
 which tesseract
+tesseract --version
